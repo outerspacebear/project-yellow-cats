52e78a5b9258ea131dbce77c2309eb497202297a
Robert Starzycki <robert.starzycki@king.com>
2020-02-01 09:38:37 +0000

    working on the plant

---
+++ Assets/Plant.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public Vector3 lastSegmentPosition = Vector2.negativeInfinity;
+    public float segmentLength = 1.0f;
+
+    public Stem mainStem;
+
+    private void Start()
+    {
+        mainStem = GetComponent<Stem>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastSegmentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lastSegmentPosition.z = 0;
+
+            var stemPositions = new List<Vector3>();
+            stemPositions.Add(lastSegmentPosition);
+
+            mainStem.SetStemPositions(stemPositions);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            var newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPosition.z = 0;
+
+            var delta = newPosition - lastSegmentPosition;
+
+            if (delta.magnitude > segmentLength)
+            {
+                lastSegmentPosition = newPosition;
+                mainStem.AddStepPosition(newPosition);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            mainStem.RemoveStemPosition();
+        }
+    }
+
+
+}
---
+++ Assets/Stem.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stem : MonoBehaviour
+{
+    public Color c1 = new Color(0, 0.4f, 0);
+    public Color c2 = new Color(0, 0.8f, 0);
+    LineRenderer lineRenderer;
+
+
+    public int shakeLastSegments = 10;
+    public float shakeStrength = 5.0f;
+
+    public float currentShakeDirection = 0.0f;
+
+    public List<Vector3> stemPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = 1.0f;
+        lineRenderer.positionCount = 0;
+        lineRenderer.startWidth = 1.0f;
+        lineRenderer.endWidth = 0.1f;
+
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        lineRenderer.colorGradient = gradient;
+    }
+
+    public void SetStemPositions(List<Vector3> positions)
+    {
+        stemPositions = positions;
+    }
+
+    public void AddStepPosition(Vector3 position)
+    {
+        stemPositions.Add(position);
+    }
+
+    public void RemoveStemPosition()
+    {
+        if (stemPositions.Count > 0)
+        {
+            stemPositions.RemoveAt(stemPositions.Count - 1);
+        }
+    }
+
+    void Update()
+    {
+        // Start from the second segment so we don't shake the base
+        int shakeStartsFrom = Mathf.Max(stemPositions.Count - shakeLastSegments, 1);
+
+        lineRenderer.positionCount = stemPositions.Count;
+        float localShakeStrength = 0;
+        for (int i = 0; i < stemPositions.Count; ++i)
+        {
+            if (i < shakeStartsFrom)
+            {
+                lineRenderer.SetPosition(i, stemPositions[i]);
+            }
+            else
+            {
+                var growthDirection = (stemPositions[i] - stemPositions[i - 1]);
+                var shakeDirection = new Vector3(growthDirection.y, -growthDirection.x, growthDirection.z).normalized;
+
+                localShakeStrength += currentShakeDirection * shakeStrength;
+                var localShake = shakeDirection * localShakeStrength;
+
+                lineRenderer.SetPosition(i, lineRenderer.GetPosition(i - 1) + growthDirection + localShake);
+
+                Debug.DrawLine(lineRenderer.GetPosition(i-1), lineRenderer.GetPosition(i), Color.red);
+                Debug.DrawLine(lineRenderer.GetPosition(i), lineRenderer.GetPosition(i) + localShake, Color.blue);
+            }
+        }
+        Debug.Log("Shaking from " + shakeStartsFrom + " localShakeStrength " + localShakeStrength);
+    }
+}
