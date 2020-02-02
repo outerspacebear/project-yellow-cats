ee801d0461b7e79c3ab11f8a3308415fbce240a7
Robert Starzycki <robert.starzycki@king.com>
2020-02-02 14:11:01 +0000

    add seed spawner

---
+++ Assets/Scripts/SeedSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedSpawner : MonoBehaviour
+{
+    public Transform spawnPosition;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Seed"))
+        {
+            collision.gameObject.transform.position = spawnPosition.position;
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+
+        }
+    }
+}
