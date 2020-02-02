fc94d9ff70ccae9caec1dd237fcf107b52fbe66b
Viraj <virjune@gmail.com>
2020-02-02 01:45:36 +0000

    Added cinematic zoom-out start

---
+++ Assets/Scripts/CameraZoomer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraZoomer : MonoBehaviour
+{
+    [SerializeField] Vector3 targetPosition;
+    [SerializeField] float targetSize;
+    [SerializeField] float moveRate;
+    [SerializeField] float zoomRate;
+    [SerializeField] float threshold = 0.02f;
+    private Camera camera;
+    private bool shouldZoom = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            shouldZoom = true;
+        }
+        if (shouldZoom)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, moveRate);
+            camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, targetSize, zoomRate);
+            camera.Render();
+
+            if(Vector2.Distance(transform.position, targetPosition) < threshold && Mathf.Abs(camera.orthographicSize - targetSize) < threshold)
+            {
+                transform.position = targetPosition;
+                camera.orthographicSize = targetSize;
+                Destroy(this);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GravityToggler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityToggler : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rigidbody;
+    [SerializeField] float startGravity = 0f;
+    [SerializeField] float targetGravity = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.gravityScale = startGravity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            rigidbody.gravityScale = targetGravity;
+            Destroy(this);
+        }
+    }
+}
